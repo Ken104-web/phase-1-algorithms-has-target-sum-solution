627b471ca0d55e5f78827568aab375443bdd21f6
KenCorp <kennethwarui35@gmail.com>
2023-07-10 08:27:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function hasTargetSum(array, target) {
+
+  function hasTargetSum(array, target) {
   // Write your algorithm here
+  const complementMap = new Map();
+
+  for (let i = 0; i < array.length; i++){
+    const num = array[i];
+    const complement = target - num;
+
+    if(complementMap.has(complement)){
+      return true;
+    }
+    complementMap.set(num, i);
+  }
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
